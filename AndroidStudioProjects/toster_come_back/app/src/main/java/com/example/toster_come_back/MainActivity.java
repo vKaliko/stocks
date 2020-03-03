de42b13d1b5c8249a0effdf3e9ffe868c3170b93
John Kaliko <vkaliko@gmail.com>
2020-03-03 19:16:31 +0300

    Empty tabbed app with firebase sdk

---
+++ AndroidStudioProjects/toster_come_back/app/src/main/java/com/example/toster_come_back/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.toster_come_back;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
